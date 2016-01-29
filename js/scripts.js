293d3e9b29fb6724c7c0e457f66ee5698c0f1dab
kevin <mattisonii@yahoo.com>
2016-01-29 09:23:02 -0800

    Add functionality for calculating cost based on pizza size.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 //BDD
 
 //constructor for PizzaOrder. Includes size and an empty array for toppings.
-function PizzaOrder(size, toppings) {
+function PizzaOrder(firstName, lastName, size, toppings) {
+  this.firstName = firstName;
+  this.lastName = lastName;
   this.size = size;
   this.toppings = [];
 };
+
+PizzaOrder.prototype.pizzaCost = function() {
+  if (this.size = "small") {
+    return "$5";
+  }
+};
+
+
+//User Interface
+
+// $(document).ready(function(){
+//
+//
+//   //Submit form when order button is clicked
+//   $(#submitOrder).click(function(event) {
+//
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,16 @@
 describe("PizzaOrder", function() {
-  it("will provide a value for the size of the pizza and toppings", function() {
-    var newPizzaOrder = new PizzaOrder("Large");
+  it("will provide a value for the pizza order", function() {
+    var newPizzaOrder = new PizzaOrder("John", "Public", "Large");
+    expect(newPizzaOrder.firstName).to.equal("John");
+    expect(newPizzaOrder.lastName).to.equal("Public");
     expect(newPizzaOrder.size).to.equal("Large");
     expect(newPizzaOrder.toppings).to.eql([]);
+  });
+});
+
+describe("pizzaCost", function() {
+  it("will provide a cost of $5 for a personal pan pizza", function() {
+    var newPizzaOrder = new PizzaOrder("John", "Public", "personal");
+    expect(newPizzaOrder.pizzaCost()).to.equal("$5");
   });
 });
